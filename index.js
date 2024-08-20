78c31d2fa29b473b9af21c4c5cae76f8cbc2fb75
“Abdul <kaazees31@gmail.com>
2024-08-20 19:16:48 +0530

    update

--- index.js
+++ index.js
@@ -19,9 +19,7 @@ const PORT = process.env.PORT || 5000;
 // Middlewares
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cors({
-    origin: 'http://localhost:3000',
-}));
+app.use(cors());
 
 // Routes
 app.get('/', (req, res) => {
